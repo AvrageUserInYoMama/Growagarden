6ed2577dafcb65d8f9350210849851eee759ae06
AvrageUserInYoMama <152017676+AvrageUserInYoMama@users.noreply.github.com>
2025-05-29 16:42:28 +0100

    Update For Public Useage.py

--- For Public Useage.py
+++ For Public Useage.py
@@ -65,7 +65,7 @@ PRICE_PER_KG = {
     "Watermelon": 80,
     "Cactus": 1110,
     "Passionfruit": 1400,
-    "Pepper": 1700,
+    "Pepper": 2300,
     "Starfruit": 5611,
     "Moonflower": 4000,
     "Moonglow": 3400,
